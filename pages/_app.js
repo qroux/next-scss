c57eae3c8faa2ae1b6e302abf0bdc9980aa1309d
Quentin Roux <quentin.roux@hotmail.fr>
2020-10-07 19:47:17 +0200

    sass loader configured

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/global.scss";
+import styles from "../styles/Layout.module.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.layout}>
+        <div className={styles.navbar}>
+          <div className={styles.navbar__item}>1</div>
+          <div className={styles.navbar__item}>2</div>
+          <div className={styles.navbar__item}>3</div>
+        </div>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
