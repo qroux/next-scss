653322673784172e0a9d9c3e721f1e642d81ffec
qroux <quentin.roux@hotmail.fr>
2021-09-30 19:26:17 +0200

    Navbar links refacto

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,35 @@
-import Link from "next/link";
-import styles from "../styles/components/Navbar.module.scss";
+import Link from 'next/link';
+import styles from '../styles/components/Navbar.module.scss';
+
+const links = [
+  { path: '#presentation', label: 'Présentation' },
+  { path: '#skills', label: 'Compétences' },
+  { path: '#contacts', label: 'Contacts' },
+];
 
 export default function Navbar() {
+  const renderLinks = () => {
+    return links.map((link, id) => {
+      return (
+        <Link href={link.path} passHref key={id}>
+          <a className={styles.navbar__item}>{link.label}</a>
+        </Link>
+      );
+    });
+  };
+
   return (
     <div className={styles.navbar}>
       <div className={styles.navbar__right}>
-        <Link href="/">
-          <img src="/cube.svg" alt="blockchain logo" className={styles.navbar__logo}/>
+        <Link href='/'>
+          <img
+            src='/cube.svg'
+            alt='blockchain logo'
+            className={styles.navbar__logo}
+          />
         </Link>
       </div>
-      <div className={styles.navbar__left}>
-        {/* <Link href="/presentation">
-          <div className={styles.navbar__item}>Présentation</div>
-        </Link>
-        <Link href="/languages">
-          <div className={styles.navbar__item}>Languages</div>
-        </Link> */}
-        <a href="#presentation" className={styles.navbar__item}>
-          Présentation
-        </a>  
-        <a href="#skills" className={styles.navbar__item}>
-          Compétences
-        </a>  
-        <a href="#contacts" className={styles.navbar__item}>
-          Contacts
-        </a>     
-      </div>
- 
+      <div className={styles.navbar__left}>{renderLinks()}</div>
     </div>
   );
 }
