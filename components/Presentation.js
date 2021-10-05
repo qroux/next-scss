46a56e667fab24b753be4452f6a860d10fa69908
qroux <quentin.roux@hotmail.fr>
2021-10-05 03:15:09 +0200

    Next + react + sass updated to latest to fix css module  build issue

--- components/Presentation.js
+++ components/Presentation.js
@@ -1,37 +1,37 @@
-import styles from '../styles/components/Presentation.module.scss';
-import Link from 'next/link';
-import EthSvg from '../public/ethereum-2.svg';
+import styles from "../styles/components/Presentation.module.scss";
+import Link from "next/link";
+// import EthSvg from "../public/ethereum-2.svg";
 // import Coding from "../public/coding.svg";
-import LeWagon from '../public/lewagon.png';
-import WebDev from '../public/web-dev-2.svg';
-import { motion } from 'framer-motion';
-import { InView } from 'react-intersection-observer';
-import { Animations } from '../styles/framerAnimations';
-import { ChevronSvg } from './ChevronSvg';
+// import LeWagon from '../public/lewagon.png';
+// import WebDev from '../public/web-dev-2.svg';
+import { motion } from "framer-motion";
+import { InView } from "react-intersection-observer";
+import { Animations } from "../styles/framerAnimations";
+import { ChevronSvg } from "./ChevronSvg";
 
 const questions = [
   {
-    id: 'first',
-    title: 'Pourquoi',
-    logo: EthSvg,
+    id: "first",
+    title: "Pourquoi",
+    // logo: EthSvg,
     content:
-      'Juriste de formation, je suis finalement tombé dans la  programmation en cherchant à ',
-    span: 'coder des smartcontracts sur  blockchain ethereum.',
+      "Juriste de formation, je suis finalement tombé dans la  programmation en cherchant à ",
+    span: "coder des smartcontracts sur  blockchain ethereum.",
   },
   {
-    id: 'second',
-    title: 'Comment',
-    logo: LeWagon,
+    id: "second",
+    title: "Comment",
+    // logo: LeWagon,
     content:
       "Après une période de découverte en autodidacte, j'ai fini par suivre une ",
-    span: 'formation de développeur Fullstack au Wagon Marseille (batch #212).',
+    span: "formation de développeur Fullstack au Wagon Marseille (batch #212).",
   },
   {
-    id: 'third',
-    title: 'Maintenant',
-    logo: WebDev,
-    content: 'Je développe en langage ',
-    span: 'Javascript et Typescript (Backend/Frontend).',
+    id: "third",
+    title: "Maintenant",
+    // logo: WebDev,
+    content: "Je développe en langage ",
+    span: "Javascript et Typescript (Backend/Frontend).",
   },
 ];
 
@@ -44,11 +44,12 @@ const renderText = questions.map((question) => {
             className={styles.questionBox}
             ref={ref}
             variants={Animations.sectionTransition}
-            initial={'hidden'}
-            animate={inView ? 'visible' : ''}>
+            initial={"hidden"}
+            animate={inView ? "visible" : ""}
+          >
             <h3 className={styles.question__heading}>{question.title}</h3>
             <p>
-              {question.content}{' '}
+              {question.content}{" "}
               <span className={styles.question__span}>{question.span}</span>
             </p>
           </motion.div>
@@ -62,7 +63,7 @@ export default function Presentation() {
   return (
     <div className={styles.presentation__layout}>
       <div className={styles.chevron}>
-        <Link href='#presentation'>
+        <Link href="#presentation">
           <div className={styles.chevron__box}>
             <ChevronSvg />
           </div>
@@ -77,8 +78,8 @@ export default function Presentation() {
         <div className={styles.presentation__content}>
           <div className={styles.presentation__contentText}>{renderText}</div>
           <img
-            src='/ethereum-2.svg'
-            alt='ethereum-2 logo'
+            src="/ethereum-2.svg"
+            alt="ethereum-2 logo"
             className={styles.presentation__contentImage}
           />
         </div>
